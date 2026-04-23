0531c7e7c53549b8b64500b0890a7c84f101dba6
eyadsayed55-ship-it <eiadsayed55@gmail.com>
2026-04-23 17:15:25 +0200

    Update Booking.java

--- main_classes/Booking.java
+++ main_classes/Booking.java
@@ -1,15 +1,19 @@
 package main_classes;
 
+import exceptions.InvalidDurationException;
+
 public class Booking {
     private Room room;
     private int nights;
 
-    public Booking(Room room, int nights) {
+    public Booking(Room room, int nights) throws InvalidDurationException {
+        if (nights <= 0) {
+            throw new InvalidDurationException("A booking must be for at least 1 night.");
+        }
         this.room = room;
         this.nights = nights;
     }
 
-    // Getters
     public Room getRoom() { return room; }
     public int getNights() { return nights; }
 }
