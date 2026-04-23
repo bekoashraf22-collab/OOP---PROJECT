98791e2c04818b6ad0ad7ea71fe2a74c22541290
eyadsayed55-ship-it <eiadsayed55@gmail.com>
2026-04-23 17:18:22 +0200

    Update RoomType.java

--- main_classes/RoomType.java
+++ main_classes/RoomType.java
@@ -1,27 +1,27 @@
 package main_classes;
+
+import exceptions.InvalidPricingException;
+
 public class RoomType {
     private String typeId;
-    private String typeName; // e.g., "Single", "Double", "Suite"
+    private String typeName; 
     private double basePrice;
     private int capacity;
 
-    public RoomType(String typeId, String typeName, double basePrice, int capacity) {
+    public RoomType(String typeId, String typeName, double basePrice, int capacity) throws InvalidPricingException {
+        if (basePrice < 0) throw new InvalidPricingException("Room price cannot be negative.");
         this.typeId = typeId;
         this.typeName = typeName;
         this.basePrice = basePrice;
         this.capacity = capacity;
     }
 
-    // Getters and Setters
     public String getTypeId() { return typeId; }
     public void setTypeId(String typeId) { this.typeId = typeId; }
-
     public String getTypeName() { return typeName; }
     public void setTypeName(String typeName) { this.typeName = typeName; }
-
     public double getBasePrice() { return basePrice; }
     public void setBasePrice(double basePrice) { this.basePrice = basePrice; }
-
     public int getCapacity() { return capacity; }
     public void setCapacity(int capacity) { this.capacity = capacity; }
 
@@ -29,4 +29,3 @@ public class RoomType {
     public String toString() {
         return typeName + " - $" + basePrice + "/night";
     }
-}
