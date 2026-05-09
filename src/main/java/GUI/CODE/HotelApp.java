ac6ba16c842d7709fea39f35df7241f53006f1d1
Ali sameh <ali.sameh.k@gmail.com>
2026-05-09 04:03:48 +0300

    fixed scene re sizing issue

--- src/main/java/GUI/CODE/HotelApp.java
+++ src/main/java/GUI/CODE/HotelApp.java
@@ -21,19 +21,44 @@ public class HotelApp extends Application {
     }
 
     public static void show(String fxmlPath) {
-        try {
-            Parent root = FXMLLoader.load(HotelApp.class.getResource(fxmlPath));
-            Scene scene = new Scene(root, 1280, 760);
+    try {
+        boolean wasMaximized = primaryStage.isMaximized();
+        boolean wasFullScreen = primaryStage.isFullScreen();
+
+        double currentWidth = primaryStage.getWidth();
+        double currentHeight = primaryStage.getHeight();
+        double currentX = primaryStage.getX();
+        double currentY = primaryStage.getY();
+
+        Parent root = FXMLLoader.load(HotelApp.class.getResource(fxmlPath));
+
+        Scene scene = primaryStage.getScene();
+        if (scene == null) {
+            scene = new Scene(root, 1280, 760);
             String css = HotelApp.class.getResource("/GUI/CSS/hotel.css").toExternalForm();
             scene.getStylesheets().add(css);
             primaryStage.setScene(scene);
-            primaryStage.centerOnScreen();
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Could not load " + fxmlPath, e);
+        } else {
+            scene.setRoot(root);
         }
+
+        if (!scene.getStylesheets().contains(HotelApp.class.getResource("/GUI/CSS/hotel.css").toExternalForm())) {
+            scene.getStylesheets().add(HotelApp.class.getResource("/GUI/CSS/hotel.css").toExternalForm());
+        }
+
+        primaryStage.setWidth(currentWidth);
+        primaryStage.setHeight(currentHeight);
+        primaryStage.setX(currentX);
+        primaryStage.setY(currentY);
+        primaryStage.setMaximized(wasMaximized);
+        primaryStage.setFullScreen(wasFullScreen);
+
+        primaryStage.show();
+    } catch (Exception e) {
+        e.printStackTrace();
+        throw new RuntimeException("Could not load " + fxmlPath, e);
     }
+}
 
     public static void logout() {
         AppSession.clear();
